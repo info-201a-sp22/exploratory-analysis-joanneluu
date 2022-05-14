1033ede3dd8e8e85a7e286467fe8b2a8fcbd36c0
Denny Wu <dennywu@uw.edu>
2022-05-14 14:44:33 -0700

    finish calculating all values

--- summary-information.R
+++ summary-information.R
@@ -12,12 +12,27 @@ most_popular_name <- most_popular_name %>% filter(n == max(n, na.rm = TRUE)) %>%
 most_popular_breed <- dog_data %>% group_by(BreedName) %>% 
   summarise(n = n())
 most_popular_breed <- most_popular_breed[most_popular_breed$BreedName != "Unknown", ]
-most_popular_breed <- most_popular_breed %>% filter(n == max(n, na.rm = TRUE)) %>% pull(BreedName)
+most_popular_breed <- most_popular_breed %>% filter(n == max(n, na.rm = TRUE)) %>% 
+  pull(BreedName)
 
 proportion_top_20 <- dog_data %>% group_by(BreedName) %>% 
   summarise(n = n())
-proportion_top_20 <- most_popular_breed[most_popular_breed$BreedName != "Unknown", ]
+proportion_top_20 <- proportion_top_20[proportion_top_20$BreedName != "Unknown", ]
 total_dogs <- proportion_top_20 %>% summarise(sum(n))
-proportion_top_20 <- proportion_top_20 %>% top_n(20) %>% summarise(sum(n))
+proportion_top_20 <- proportion_top_20 %>% top_n(20) %>% 
+  summarise(sum(n))
 proportion_top_20 <- proportion_top_20 / total_dogs
+proportion_top_20 <- proportion_top_20 %>% pull("sum(n)")
 
+borough_largest_population <- dog_data %>% group_by(Borough) %>% 
+  summarise(population = n()) %>% 
+  filter(population == max(population)) %>% 
+  pull(Borough)
+
+borough_most_unique_names <- dog_data %>% group_by(Borough) %>% 
+  distinct(AnimalName) %>% 
+  summarise(number_of_names = n()) %>% 
+  filter(number_of_names == max(number_of_names)) %>% 
+  pull(Borough)
+
+  
