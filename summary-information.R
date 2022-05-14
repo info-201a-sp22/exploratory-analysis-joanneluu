f1dd27fa0b6d407287fd6d36996d6cb96085356e
Denny Wu <dennywu@uw.edu>
2022-05-14 00:03:50 -0700

    add more calculations

--- summary-information.R
+++ summary-information.R
@@ -7,4 +7,17 @@ most_popular_name <- dog_data %>% group_by(AnimalName) %>%
   summarise(n = n()) 
 most_popular_name <- most_popular_name[most_popular_name$AnimalName != "UNKNOWN", ]
 most_popular_name <- most_popular_name[most_popular_name$AnimalName != "NAME NOT PROVIDED", ]
-most_popular_name <- most_popular_name %>% filter(n == max(n))
+most_popular_name <- most_popular_name %>% filter(n == max(n, na.rm = TRUE)) %>% pull(AnimalName)
+
+most_popular_breed <- dog_data %>% group_by(BreedName) %>% 
+  summarise(n = n())
+most_popular_breed <- most_popular_breed[most_popular_breed$BreedName != "Unknown", ]
+most_popular_breed <- most_popular_breed %>% filter(n == max(n, na.rm = TRUE)) %>% pull(BreedName)
+
+proportion_top_20 <- dog_data %>% group_by(BreedName) %>% 
+  summarise(n = n())
+proportion_top_20 <- most_popular_breed[most_popular_breed$BreedName != "Unknown", ]
+total_dogs <- proportion_top_20 %>% summarise(sum(n))
+proportion_top_20 <- proportion_top_20 %>% top_n(20) %>% summarise(sum(n))
+proportion_top_20 <- proportion_top_20 / total_dogs
+
