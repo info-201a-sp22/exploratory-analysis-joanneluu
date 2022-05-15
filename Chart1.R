c54fb6c11075550a5e1c8442a6b19e94d82cfb16
xiqianyuan <xiqiay@uw.edu>
2022-05-15 15:22:33 -0700

    Chart 1 modify

--- Chart1.R
+++ Chart1.R
@@ -1,4 +1,5 @@
 library(ggplot2)
+library(dplyr)
 dog_data <- read.csv("https://raw.githubusercontent.com/dennywu84/Final-Project/main/dog-data.csv", stringsAsFactors = F )
 
 dog_data <- dog_data %>% 
@@ -16,16 +17,25 @@ top_10to15 <- 1897 + 1774 + 1759 + 1751 + 1718
 top_15to20 <- 1480 + 1315 + 1289 + 1267 + 1242
 other <- nrow(dog_data) - sum(breed_n$n)
 
+total <- nrow(dog_data)
 Name <- c("top_5", "top_5to10", "top_10to15", "top_15to20", "other")
 n <- c(top_5, top_5to10, top_10to15, top_15to20, other)
+pct <- n / total
+pct <- paste(as.character(round(n/total * 100, digits = 2)), "%")
 
-data <- data.frame(Name, n)
+data <- data.frame(Name, n, pct)
 
 ggplot(data, aes(x = "", y = n, fill = Name)) +
-  geom_bar(stat="identity", width=1, color="white") +
-  coord_polar("y", start=0) +
+  geom_col() +
+  coord_polar("y") +
+  geom_text(aes(label = pct, x = 1.1), position = position_stack(vjust = 0.5), size = 3.5) +
   theme_void() + 
-  labs(title = "Comparing the percentage of the number of top 20 name")
+  labs(title = "Comparing the Percentage of the Number of Top 20 Name") +
+  scale_fill_discrete(name = "Group", 
+                      labels = c("Other", "Top 10 to 15 Name", "Top 15 to 20 Name", 
+                                 "Top 5 Name", "Top 5 to 10 Name"))
+
+
 
 
 
