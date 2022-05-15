227294545ba64bfb3bdaa0b8a830ab4cbc68ba92
Denny Wu <dennywu@uw.edu>
2022-05-14 23:47:03 -0700

    fix some bugs and put values into a list

--- summary-information.R
+++ summary-information.R
@@ -2,7 +2,6 @@ library("dplyr")
 
 dog_data <- read.csv("https://raw.githubusercontent.com/dennywu84/Final-Project/main/dog-data.csv", stringsAsFactors = F )
 
-summary_information_func <- function(dog_data) {
 summary_info <- list()
 
 most_popular_name <- dog_data %>% group_by(AnimalName) %>% 
@@ -19,12 +18,13 @@ summary_info$most_popular_breed <- most_popular_breed %>% filter(n == max(n, na.
 
 proportion_top_20 <- dog_data %>% group_by(BreedName) %>% 
   summarise(n = n())
-proportion_top_20 <- proportion_top_20[proportion_top_20$BreedName != "Unknown", ]
 total_dogs <- proportion_top_20 %>% summarise(sum(n))
+proportion_top_20 <- proportion_top_20[proportion_top_20$BreedName != "Unknown", ]
 proportion_top_20 <- proportion_top_20 %>% top_n(20) %>% 
   summarise(sum(n))
 proportion_top_20 <- proportion_top_20 / total_dogs
-summary_info$proportion_top_20 <- proportion_top_20 %>% pull("sum(n)")
+summary_info$proportion_top_20 <- percent(proportion_top_20 %>% pull("sum(n)"), .01)
+
 
 summary_info$borough_largest_population <- dog_data %>% group_by(Borough) %>% 
   summarise(population = n()) %>% 
@@ -36,8 +36,7 @@ summary_info$borough_most_unique_names <- dog_data %>% group_by(Borough) %>%
   summarise(number_of_names = n()) %>% 
   filter(number_of_names == max(number_of_names)) %>% 
   pull(Borough)
-}
 
-summary_information_func(dog_data)
+
 
   
